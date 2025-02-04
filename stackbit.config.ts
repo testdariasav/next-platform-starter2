9b4206b67ac0351bc111b92f148bb44df0eeb0a0
DariaS <testdariasav@gmail.com>
2025-02-04 13:56:06 -0500

    Revert "Update stackbit.config.ts"
    This reverts commit f0592e9789b100be67d8d73e4d6343383713bc2c.

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -20,35 +20,5 @@ export default defineStackbitConfig({
       ],
     })],
     "postInstallCommand": "npm i --no-save @stackbit/types",
-      siteMap: ({ documents, models }) => {
-    // 1. Filter all page models
-    const pageModels = models.filter((m) => m.type === "page")
-
-    return documents
-      // 2. Filter all documents which are of a page model
-      .filter((d) => pageModels.some(m => m.name === d.modelName))
-      // 3. Map each document to a SiteMapEntry
-      .map((document) => {
-        // Map the model name to its corresponding URL
-        const urlModel = (() => {
-            switch (document.modelName) {
-                case 'Page':
-                    return 'otherPage';
-                case 'Blog':
-                    return 'otherBlog';
-                default:
-                    return null;
-            }
-        })();
-
-        return {
-          stableId: document.id,
-          urlPath: `/${urlModel}/${document.id}`,
-          document,
-          isHomePage: false,
-        };
-      })
-      .filter(Boolean) as SiteMapEntry[];
-  }
 })
 
